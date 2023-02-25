42d139dc0737cb11a3c84fbcc09fed0595d413da
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-25 12:46:12 +0600

    fetch jsonplaceholder data

--- js/fetch.js
+++ js/fetch.js
@@ -1,1 +1,16 @@
-console.log("Fetching");
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => console.log(response)) // .json is not similar but close to JSON.parse
+//       .then(json => console.log(json))
+
+const url = "https://sonplaceholder.typicode.com/todos/1";
+
+// fetch(url)
+// .then(response =>response.json())
+// .then(json => console.log(json))
+
+function loadData() {
+    const url='https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
